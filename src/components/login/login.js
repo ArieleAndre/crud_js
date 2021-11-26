c13a8946480a83fdb9b1934ee924b7402b1b9a77
arieleandre <arielebrites15@gmail.com>
2021-11-25 21:52:25 -0300

    Login Atualizado

---
+++ src/components/login/login.js
@@ -0,0 +1,6 @@
+document.querySelector(".button").addEventListener("click", load);
+
+function load() {
+    var trocarCor = document.querySelector(".button")
+    trocarCor.style.backgroundColor = "#1e111d"
+}
